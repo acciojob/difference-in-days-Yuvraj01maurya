a2fed18aa025708f831a35eb45a2b35acab54869
Yuvraj Maurya <yuvraj95166@gmail.com>
2023-12-06 20:32:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,35 +1,23 @@
 var dateDiffInDays = function (date1, date2) {
   //   write your code here
-  function dateDiffInDays(date1, date2) {
-  const oneDay = 24 * 60 * 60 * 1000; // Number of milliseconds in a day
+  const oneDay = 24 * 60 * 60 * 1000; // hours*minutes*seconds*milliseconds
+  const firstDate = new Date(date1);
+  const secondDate = new Date(date2);
 
-  const utc1 = Date.UTC(
-    parseInt(date1.substring(0, 4)), // Year
-    parseInt(date1.substring(5, 7)) - 1, // Month (zero-based)
-    parseInt(date1.substring(8, 10)) // Day
-  );
+  // Calculate the difference in milliseconds
+  const diffInTime = secondDate.getTime() - firstDate.getTime();
 
-  const utc2 = Date.UTC(
-    parseInt(date2.substring(0, 4)), // Year
-    parseInt(date2.substring(5, 7)) - 1, // Month (zero-based)
-    parseInt(date2.substring(8, 10)) // Day
-  );
+  // Calculate the difference in days
+  const diffInDays = Math.round(diffInTime / oneDay);
 
-  const diffDays = Math.floor((utc2 - utc1) / oneDay);
- return alert(diffDays);
+  return diffInDays;
 }
-
-// Examples
-console.log(dateDiffInDays("2022-03-01", "2022-03-15")); // Output: 14
-console.log(dateDiffInDays("2022-03-01", "2022-03-01")); // Output: 0
-console.log(dateDiffInDays("2022-03-15", "2022-03-01")); // Output: -14
-
-};
 
 // Do not change the code below.
 const dateOne = prompt("Enter Start Date.");
 const dateTwo = prompt("Enter End Date.");
 alert(dateDiffInDays(dateOne, dateTwo));
+
 
 
 
